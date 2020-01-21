14b02de2ea29a41c5ede0494efb4089be5df6365
Jimmy Rojas <jimmy.rojas@truextend.com>
2020-01-21 09:11:02 -0400

    Added main loadData functionality

--- src/main/java/com/organization/airport_operation/services/AirportOperationService.java
+++ src/main/java/com/organization/airport_operation/services/AirportOperationService.java
@@ -1,5 +1,11 @@
 package com.organization.airport_operation.services;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.organization.airport_operation.model.Airport;
+import com.organization.airport_operation.model.AirportData;
+import java.io.FileReader;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,9 +13,22 @@ import org.springframework.stereotype.Service;
 public class AirportOperationService {
 
   private ResourceFileService resourceFileService;
+  private Airport airport;
 
   @Autowired
   public AirportOperationService(ResourceFileService resourceFileService) {
     this.resourceFileService = resourceFileService;
   }
+
+  public synchronized void loadData() throws IOException {
+    Gson gson = new Gson();
+    JsonReader reader = new JsonReader(new FileReader(this.resourceFileService.getResourceFile()));
+    AirportData airportData = gson.fromJson(reader, AirportData.class);
+    if (airportData != null) {
+      airport = airportData.getAirport();
+    } else {
+      throw new IOException("Unable to load main Airport Data.");
+    }
+  }
+
 }
---
+++ src/test/java/com/organization/airport_operation/services/AirportOperationServiceTest.java
@@ -0,0 +1,53 @@
+package com.organization.airport_operation.services;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AirportOperationServiceTest {
+
+  private AirportOperationService instance;
+  private ResourceFileService resourceFileService;
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+
+    this.resourceFileService = new ResourceFileService();
+
+    Resource r = new ClassPathResource("data.json");
+    org.springframework.test.util.ReflectionTestUtils
+        .setField(this.resourceFileService, "fileResource", r);
+
+    this.resourceFileService.init();
+
+    this.instance = new AirportOperationService(this.resourceFileService);
+  }
+
+  @Test (expected = IOException.class)
+  public void loadData_invalid_data() throws Exception {
+    ResourceFileService resourceFileService = new ResourceFileService();
+
+    org.springframework.test.util.ReflectionTestUtils.setField(
+        resourceFileService, "fileResource", new ClassPathResource("invalid_data.json"));
+
+    resourceFileService.init();
+
+    AirportOperationService instance = new AirportOperationService(resourceFileService);
+    instance.loadData();
+  }
+
+  @Test
+  public void loadData() throws Exception {
+    this.instance.loadData();
+  }
+
+}
