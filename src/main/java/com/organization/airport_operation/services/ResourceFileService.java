e59d2ef69ebb3193231fda3ca8defcc4b0a938ff
Jimmy Rojas <jimmy.rojas@truextend.com>
2020-01-22 09:56:16 -0400

    Added helpful LoggerHelper

--- src/main/java/com/organization/airport_operation/services/ResourceFileService.java
+++ src/main/java/com/organization/airport_operation/services/ResourceFileService.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import com.organization.airport_operation.model.AirportData;
 import com.organization.airport_operation.utils.FileUtils;
+import com.organization.airport_operation.utils.LoggerHelper;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -30,9 +31,15 @@ public class ResourceFileService implements IAirportDataSource {
 
   @Override
   public AirportData loadAirportData() throws FileNotFoundException {
+    long startProcessingTime = System.currentTimeMillis();
+
     Gson gson = new Gson();
     JsonReader reader = new JsonReader(new FileReader(this.resourceFile));
     AirportData airportData = gson.fromJson(reader, AirportData.class);
+
+    long totalProcessingTimeThreads = System.currentTimeMillis() - startProcessingTime;
+    LoggerHelper.info("loadAirportData Ended: " + totalProcessingTimeThreads);
+
     return airportData;
   }
 }
